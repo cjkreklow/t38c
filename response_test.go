c08331968021897064129049f98cfeffbdc149d1
Collin Kreklow <collin@kreklow.us>
2020-10-02 12:16:32 -0500

    Lint cleanup - response_test.go

--- response_test.go
+++ response_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Collin Kreklow
+// Copyright 2020 Collin Kreklow
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -20,122 +20,240 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-package t38c
+package t38c_test
 
-import "testing"
+import (
+	"testing"
 
-// TestResponseInvalid simulates several error conditions
-func TestResponseInvalid(t *testing.T) {
-	var err error
-	r := new(Response)
-	err = r.UnmarshalText([]byte(`{invalid}`))
-	if err == nil || err.Error() != "received invalid JSON" {
-		t.Errorf("Received: %s\nExpected: received invalid JSON\n", err)
-	}
+	"kreklow.us/go/t38c"
+)
 
-	r = new(Response)
-	err = r.UnmarshalText([]byte(`{"zzz":true}`))
-	if err == nil || err.Error() != "unknown response value" {
-		t.Errorf("Received: %s\nExpected: unknown response value\n", err)
-	}
+// TestResponseErrors tests errors returned from Response.
+func TestResponseErrors(t *testing.T) {
+	t.Run("Invalid JSON", testResponseErrJSON)
+	t.Run("Unknown Value", testResponseErrValue)
+	t.Run("Unknown Type", testResponseErrType)
+}
 
-	r = new(Response)
-	err = r.UnmarshalText([]byte(`{"fields":true}`))
-	if err == nil || err.Error() != "unknown field type" {
-		t.Errorf("Received: %s\nExpected: unknown field type\n", err)
+func testResponseErrJSON(t *testing.T) {
+	json := []byte(`{invalid}`)
+	err := "received invalid JSON"
+
+	testResponseErr(t, json, err)
+}
+
+func testResponseErrValue(t *testing.T) {
+	json := []byte(`{"zzz": true}`)
+	err := "error decoding response: unknown response value"
+
+	testResponseErr(t, json, err)
+}
+
+func testResponseErrType(t *testing.T) {
+	json := []byte(`{"fields": true}`)
+	err := "error decoding response: unknown field type"
+
+	testResponseErr(t, json, err)
+}
+
+func testResponseErr(t *testing.T, json []byte, e string) {
+	r := new(t38c.Response)
+
+	err := r.UnmarshalText(json)
+	if err == nil {
+		tErrorStr(t, "UnmarshalText", "error", "nil")
+	} else if e != err.Error() {
+		tErrorStr(t, "UnmarshalText", e, err)
 	}
 }
 
-// TestResponseSingle simulates a single object GET
-func TestResponseSingle(t *testing.T) {
-	var err error
-	r := new(Response)
-	err = r.UnmarshalText([]byte(`{"ok":true,"object":{"type":"Point","coordinates":[0,0]},"fields":{"fY":999.999,"fZ":123},"elapsed":"100ms"}`))
-	if err != nil {
-		t.Errorf("Received unxpected error: %s\n", err)
-	}
-	if !r.Ok {
-		t.Error("Ok not true\n")
-	}
+// TestResponseValid tests valid Responses.
+func TestResponseValid(t *testing.T) {
+	t.Run("Single JSON", testResponseSingleJSON)
+	t.Run("Single String", testResponseSingleString)
+	t.Run("Multiple Objects", testResponseMultObj)
+	t.Run("Multiple IDs", testResponseMultID)
+	t.Run("Geofence", testResponseFence)
+	t.Run("Server Error", testResponseSrvErr)
+}
+
+func testResponseSingleJSON(t *testing.T) {
+	json := []byte(`{"ok":true,"object":{"type":"Point","coordinates":[0,0]},"fields":{"fY":999.999,"fZ":123},"ttl":500,"elapsed":"100ms"}`)
 	obj := `{"type":"Point","coordinates":[0,0]}`
-	if r.Object != obj {
-		t.Errorf("Received: %s\nExpected: %s\n", r.Object, obj)
+
+	testResponseSingle(t, json, obj)
+}
+
+func testResponseSingleString(t *testing.T) {
+	json := []byte(`{"ok":true,"object":"objstr","fields":{"fY":999.999,"fZ":123},"ttl":500,"elapsed":"100ms"}`)
+	obj := `objstr`
+
+	testResponseSingle(t, json, obj)
+}
+
+func testResponseSingle(t *testing.T, json []byte, obj string) {
+	r := new(t38c.Response)
+
+	err := r.UnmarshalText(json)
+	if err != nil {
+		tFatalErr(t, "UnmarshalText", err)
 	}
+
+	if !r.Ok {
+		tErrorStr(t, "Ok", "true", "false")
+	}
+
+	if r.Object != obj {
+		tErrorStr(t, "Object", obj, r.Object)
+	}
+
 	fields := map[string]float64{
 		"fY": 999.999,
 		"fZ": 123,
 	}
 	for k, v := range r.FieldNames {
 		if r.FieldValues[v] != fields[k] {
-			t.Errorf("Received %s: %f\nExpected: %f\n", k, r.FieldValues[v], fields[k])
+			tErrorVal(t, k, fields[k], r.FieldValues[v])
 		}
 	}
-	if r.Elapsed != "100ms" {
-		t.Errorf("Received: %s\nExpected: %s\n", r.Elapsed, "100ms")
+
+	expTTL := float64(500)
+	if r.TTL != expTTL {
+		tErrorVal(t, "TTL", expTTL, r.TTL)
+	}
+
+	expElapsed := "100ms"
+	if r.Elapsed != expElapsed {
+		tErrorStr(t, "Elapsed", expElapsed, r.Elapsed)
 	}
 }
 
-// TestResponseMultiple simulates a multiple object SCAN
-func TestResponseMultiple(t *testing.T) {
-	var err error
-	r := new(Response)
-	err = r.UnmarshalText([]byte(`{"ok":true,"fields":["fZ","fY"],"objects":[{"id":"value1","object":{"type":"Point","coordinates":[0,0]},"fields":[123,999]},{"id":"value2","object":{"type":"Point","coordinates":[45,45]},"fields":[0,456]}],"count":2,"cursor":1,"elapsed":"567.89µs"}`))
-	if err != nil {
-		t.Errorf("Received unxpected error: %s\n", err)
-	}
-	if !r.Ok {
-		t.Error("Ok not true\n")
-	}
+func testResponseMultObj(t *testing.T) {
+	json := []byte(`{"ok":true,"fields":["fZ","fY"],"objects":[{"id":"value1","object":{"type":"Point","coordinates":[0,0]},"fields":[123,999]},{"id":"value2","object":{"type":"Point","coordinates":[45,45]},"fields":[0,456]}],"count":2,"cursor":0,"elapsed":"567.89µs"}`)
 	objs := []string{
 		`{"id":"value1","object":{"type":"Point","coordinates":[0,0]},"fields":[123,999]}`,
 		`{"id":"value2","object":{"type":"Point","coordinates":[45,45]},"fields":[0,456]}`,
 	}
-	if r.Objects[0] != objs[0] {
-		t.Errorf("Received: %s\nExpected: %s\n", r.Objects[0], objs[0])
-	}
-	if r.Objects[1] != objs[1] {
-		t.Errorf("Received: %s\nExpected: %s\n", r.Objects[1], objs[1])
-	}
-	if r.Count != 2 {
-		t.Errorf("Received: %d\nExpected: %d\n", r.Count, 2)
-	}
-	if r.Cursor != 1 {
-		t.Errorf("Received: %d\nExpected: %d\n", r.Cursor, 1)
-	}
-	if r.Elapsed != "567.89µs" {
-		t.Errorf("Received: %s\nExpected: %s\n", r.Elapsed, "567.89µs")
+
+	testResponseMult(t, json, objs, nil)
+}
+
+func testResponseMultID(t *testing.T) {
+	json := []byte(`{"ok":true,"ids":["value1","value2"],"count":2,"cursor":0,"elapsed":"567.89µs"}`)
+	ids := []string{"value1", "value2"}
+
+	testResponseMult(t, json, nil, ids)
+}
+
+func testResponseMult(t *testing.T, json []byte, objs []string, ids []string) {
+	r := new(t38c.Response)
+
+	err := r.UnmarshalText(json)
+	if err != nil {
+		tFatalErr(t, "UnmarshalText", err)
 	}
 
-	r = new(Response)
-	err = r.UnmarshalText([]byte(`{"ok":true,"ids":["value1","value2"],"count":2,"cursor":0,"elapsed":"1.7s"}`))
-	if err != nil {
-		t.Errorf("Received unxpected error: %s\n", err)
+	if !r.Ok {
+		tErrorStr(t, "Ok", "true", "false")
 	}
-	if r.IDs[0] != "value1" {
-		t.Errorf("Received: %s\nExpected: %s\n", r.IDs[0], "value1")
+
+	if objs != nil {
+		if len(r.Objects) < 2 {
+			t.Fatal("not enough objects returned")
+		}
+
+		if r.Objects[0] != objs[0] {
+			tErrorStr(t, "Object 0", objs[0], r.Objects[0])
+		}
+
+		if r.Objects[1] != objs[1] {
+			tErrorStr(t, "Object 1", objs[1], r.Objects[1])
+		}
 	}
-	if r.IDs[1] != "value2" {
-		t.Errorf("Received: %s\nExpected: %s\n", r.IDs[1], "value2")
+
+	if ids != nil {
+		if len(r.IDs) < 2 {
+			t.Fatal("not enough IDs returned")
+		}
+
+		if r.IDs[0] != ids[0] {
+			tErrorStr(t, "ID 0", ids[0], r.IDs[0])
+		}
+
+		if r.IDs[1] != ids[1] {
+			tErrorStr(t, "ID 1", ids[1], r.IDs[1])
+		}
+	}
+
+	expCount := int64(2)
+	if r.Count != expCount {
+		tErrorVal(t, "Count", expCount, r.Count)
+	}
+
+	expCursor := int64(0)
+	if r.Cursor != expCursor {
+		tErrorVal(t, "Cursor", expCursor, r.Cursor)
+	}
+
+	expElapsed := "567.89µs"
+	if r.Elapsed != expElapsed {
+		tErrorVal(t, "Elapsed", expElapsed, r.Elapsed)
 	}
 }
 
-// TestResponseFence simulates receiving messages from a live geofence
-func TestResponseFence(t *testing.T) {
-	var err error
+func testResponseFence(t *testing.T) {
 	msgs := [][]byte{
 		[]byte(`{"ok":true,"live":true}`),
 		[]byte(`{"command":"set","group":"5b844beb0a1c1f009ac75639","detect":"enter","key":"test","time":"2018-08-27T19:07:23.578553343Z","id":"value3","object":{"type":"Point","coordinates":[30,30,800]},"fields":{}}`),
 		[]byte(`{"command":"set","group":"5b844beb0a1c1f009ac75639","detect":"inside","key":"test","time":"2018-08-27T19:07:23.578553343Z","id":"value3","object":{"type":"Point","coordinates":[30,30,800]},"fields":{}}`),
 		[]byte(`{"command":"del","id":"value3","time":"2018-08-27T19:07:33.671191005Z"}`),
 	}
+
 	for _, m := range msgs {
-		var r = new(Response)
-		err = r.UnmarshalText(m)
+		r := new(t38c.Response)
+
+		err := r.UnmarshalText(m)
 		if err != nil {
-			t.Errorf("Received unxpected error: %s\n", err)
+			tFatalErr(t, "UnmarshalText", err)
 		}
+
 		if !r.Ok && r.Command == "" {
-			t.Error("Ok not true and Command not populated\n")
+			t.Error("Ok not true and Command not populated")
 		}
 	}
+}
+
+func testResponseSrvErr(t *testing.T) {
+	json := []byte(`{"ok":true,"err":"bad request"}`)
+
+	r := new(t38c.Response)
+
+	err := r.UnmarshalText(json)
+	if err != nil {
+		tFatalErr(t, "UnmarshalText", err)
+	}
+
+	if !r.Ok {
+		tErrorStr(t, "Ok", "true", "false")
+	}
+
+	expErr := "bad request"
+	if expErr != r.Err {
+		tErrorStr(t, "Err", expErr, r.Err)
+	}
+}
+
+func tFatalErr(t *testing.T, desc string, err error) { //nolint:unparam // future use
+	t.Helper()
+	t.Fatalf("%s: received unexpected error: %s", desc, err)
+}
+
+func tErrorStr(t *testing.T, desc string, exp interface{}, act interface{}) {
+	t.Helper()
+	t.Errorf("%s: expected %s | received %s", desc, exp, act)
+}
+
+func tErrorVal(t *testing.T, desc string, exp interface{}, act interface{}) {
+	t.Helper()
+	t.Errorf("%s: expected %v | received %v", desc, exp, act)
 }
