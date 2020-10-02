ecc216ea90fc16d674e453d94755a7ffbd648cb0
Collin Kreklow <collin@kreklow.us>
2020-10-02 15:18:52 -0500

    Add t38cError

--- t38c.go
+++ t38c.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Collin Kreklow
+// Copyright 2020 Collin Kreklow
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -20,7 +20,43 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// Package t38c implements simple convenience methods for accessing a
-// Tile38 database.
-//
+// Package t38c implements client for working with a Tile38 database.
 package t38c
+
+import "fmt"
+
+// t38cError is the error type for the t38c library.
+type t38cError struct {
+	msg  string // error message string from this library
+	werr error  // wrapped error from downstream function
+}
+
+// Error returns the string value of an error.
+func (e *t38cError) Error() string {
+	if e.werr == nil {
+		return e.msg
+	}
+
+	return e.msg + ": " + e.werr.Error()
+}
+
+// Unwrap returns an underlying error if applicable.
+func (e *t38cError) Unwrap() error {
+	return e.werr
+}
+
+// newError returns a new t38cError.
+func newError(w error, m string) *t38cError {
+	return &t38cError{
+		msg:  m,
+		werr: w,
+	}
+}
+
+// newErrorf returns a new t38cError with a Printf-style message.
+func newErrorf(w error, m string, v ...interface{}) *t38cError {
+	return &t38cError{
+		msg:  fmt.Sprintf(m, v...),
+		werr: w,
+	}
+}
